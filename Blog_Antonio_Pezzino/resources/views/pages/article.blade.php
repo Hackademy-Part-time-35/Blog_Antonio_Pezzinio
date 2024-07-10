426898c34d1b39b111607400941940de9b6fddba
Antonio Pezzino <antopez1984@gmail.com>
2024-07-10 08:29:01 +0200

    risolti errori

--- Blog_Antonio_Pezzino/resources/views/pages/article.blade.php
+++ Blog_Antonio_Pezzino/resources/views/pages/article.blade.php
@@ -1,48 +1,13 @@
-<x-layout :title="$title">
+<x-layout :title="$article['title']">
 
 
-    <h1 class="title_1">{{$title}}</h1>
-
-
-
+    <a href="{{ route('articles') }}">Indietro</a>
     
-    
-        
+    <h1>{{ $article['title'] }}</h1>
 
-@if ($articles)
+    <h2>{{ $article['category'] }}</h2>
 
-
-
-
-    <div class="container">
-        <div class="row g-3 mt-5">
-            @foreach($articles as $article)
-            @if($article['visible'])
-            <div class="col-lg-3">
-                <x-card
-                    :category="$article->category->name"
-                    :title="$article['title']"
-                    :route="route('articles.show', $article->id)"
-                    />
-            </div>
-            @endif
-        @endforeach
-        </div>
-    </div>
-
-        @else
-
-        <p>Nessun articolo disponibile</p>
-
-
-@endif
-
-
-
-
-</div>
-</div>
-
+    <p>{{ $article['description'] }}</p>
 
 
 
