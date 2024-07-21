7bb9e19349cb96b42896aa6d4c236c38044e3207
Antonio Pezzino <antopez1984@gmail.com>
2024-07-21 21:30:04 +0200

    correzione ok, es17

--- Blog_Antonio_Pezzino/app/Livewire/SearchUsers.php
+++ Blog_Antonio_Pezzino/app/Livewire/SearchUsers.php
@@ -6,12 +6,11 @@ use Livewire\Component;
 
 class SearchUsers extends Component
 {
-    public $search;
 
     public function render()
     {
         return view('livewire.search-users', [
-            'users' => \App\Models\User::search($this->search),
+            'users' => \App\Models\User::all(),
         ]);
     }
 }
