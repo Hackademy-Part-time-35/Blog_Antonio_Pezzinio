7ab37b86d28b687bc1fbe78cbc197e118e69cb2d
Antonio Pezzino <antopez1984@gmail.com>
2024-07-23 14:06:45 +0200

    aggiunte

--- Blog_Antonio_Pezzino/app/Livewire/UserForm.php
+++ Blog_Antonio_Pezzino/app/Livewire/UserForm.php
@@ -4,23 +4,41 @@ namespace App\Livewire;
 
 use Livewire\Component;
 use App\Models\User;
+use Livewire\Attributes\Validate;
 
 class UserForm extends Component
 {
+    
+    #[Validate('required')]
     public $name;
 
+    #[Validate('required')]
     public $email;
 
+    #[Validate('required')]
     public $password;
 
     public function submit()
     {
+
+        $this->validate();
+
         User::create([
             'name' => $this->name,
             'email' => $this->email,
             'password' => $this->password,
-
         ]);
+
+        session()->flash('success', 'Nuovo utente creato.');
+
+        $this->resetForm();
+    }
+
+    public function resetForm()
+    {
+        $this->name ='';
+        $this->email ='';
+        $this->password ='';
     }
 
     public function render()
--- Blog_Antonio_Pezzino/resources/views/livewire/user-form.blade.php
+++ Blog_Antonio_Pezzino/resources/views/livewire/user-form.blade.php
@@ -2,19 +2,24 @@
     <div class="border border-rounded border-rounded-3 p-3">
         <h4 class="mb-3">Crea Utente</h4>
 
+        <x-success />
+
         <form wire:submit="submit">
             <div class="row g-3">
                 <div class="col-12">
                     <label>Nome</label>
                     <input type="text" class="form-control" wire:model="name">
+                    @error('name') <span class="small text-danger">{{ $message }}</span>@enderror
                 </div>
                 <div class="col-12">
                     <label>Email</label>
                     <input type="email" class="form-control" wire:model="email">
+                    @error('email') <span class="small text-danger">{{ $message }}</span>@enderror
                 </div>
                 <div class="col-12">
                     <label>Password</label>
                     <input type="password" class="form-control" wire:model="password">
+                    @error('password') <span class="small text-danger">{{ $message }}</span>@enderror
                 </div>
                 <div>
                     <button type="submit" class="btn btn-primary">Crea Utente</button>
